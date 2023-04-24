e9b3dbe184bb03061a82c173e45c4fb54d361c93
Pouya Kary ✨ <kary@gnu.org>
2023-04-24 21:51:35 +0330

    added arc colors

--- extension/block.js
+++ extension/block.js
@@ -3,17 +3,32 @@
 // ─── BLOCK WEBSITE ──────────────────────────────────────────────────────────────
 //
 
-    document.body.innerHTML = (`
-        <div style="padding-top: calc( 40vh - 20pt);">
-            <center>
-                <div style="font-size: 70pt;">🦑</div>
-                <div style="font-family: Avenir; font-size: 20pt;">Not a healthy website!</div>
-            </center>
-        </div>
-    `)
+document.body.innerHTML = (`
+    <style>
+      @media (prefers-color-scheme: light) {
+        body {
+          background-color: var(--arc-palette-minContrastColor) !important;
+          color:            var(--arc-palette-maxContrastColor) !important;
+        }
+      }
 
-    document.head.innerHTML = (`
-        <title>Woops!</title>
-    `)
+      @media (prefers-color-scheme: dark) {
+        body {
+          background-color: var(--arc-palette-backgroundExtra)      !important;
+          color:            var(--arc-palette-foregroundSecondary)  !important;
+        }
+      }
+    </style>
+    <div style="padding-top: calc(40vh - 20pt);">
+        <center>
+            <div style="font-size: 70pt;">🌻</div>
+            <div style="font-family: Futura; font-size: 20pt;">Website Blocked~</div>
+        </center>
+    </div>
+`)
+
+document.head.innerHTML = (`
+    <title>Woops!</title>
+`)
 
 // ────────────────────────────────────────────────────────────────────────────────
